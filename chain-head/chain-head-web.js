097d95422e2dd7c2b8294aef1bb02b7cf5e6f87f
Jim Pick <jim@protocol.ai>
2020-01-21 18:30:16 -0800

    Try react-json-tree with preact (didn't work)

--- chain-head/chain-head-web.js
+++ chain-head/chain-head-web.js
@@ -2,6 +2,7 @@
 import { h, Component, render } from '/web_modules/preact.js'
 import { useEffect, useState } from '/web_modules/preact/hooks.js'
 import htm from '/web_modules/htm.js'
+import JSONTree from '/web_modules/@jimpick/preact-json-tree/umd/react-json-tree.js'
 import Client from '/lib/client.js'
 
 const html = htm.bind(h)
@@ -17,7 +18,6 @@ function ChainHead (props) {
         token: localStorage.getItem('token')
       })
       const json = await client.request('ChainHead')
-      console.log('Jim json', json)
       setChainHead(JSON.stringify(json, null, 2))
     }
     run()
@@ -26,6 +26,7 @@ function ChainHead (props) {
   return html`
     <h1>Chain Head</h1>
     <pre>${chainHead}</pre>
+    <JSONTree data=${chainHead} />
   `
 }
 
