74a0a1fd5e6cf862ee8f4bd9a8297975b9e50f86
Jim Pick <jim@protocol.ai>
2020-02-09 13:23:27 -0800

    Test reflection
    From:
    
    https://medium.com/capital-one-tech/learning-to-use-go-reflection-822a0aed74b7

--- go/js-wasm-reflect/main.go
+++ go/js-wasm-reflect/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"syscall/js"
 )
+
+type Foo struct {
+	A int `tag1:"First Tag" tag2:"Second Tag"`
+	B string
+}
 
 func printMessage(this js.Value, inputs []js.Value) interface{} {
 	message := inputs[0].String()
@@ -18,5 +24,14 @@ func printMessage(this js.Value, inputs []js.Value) interface{} {
 func main() {
 	fmt.Println("Hello, WebAssembly!")
 	js.Global().Set("printMessage", js.FuncOf(printMessage))
+	f := Foo{A: 10, B: "Salutations"}
+	fType := reflect.TypeOf(f)
+	fmt.Println("Type", fType.Name())
+	fmt.Println("Kind", fType.Kind())
+	fmt.Println("NumField", fType.NumField())
+	for i := 0; i < fType.NumField(); i++ {
+		f := fType.Field(i)
+		fmt.Printf("%v (%v)\n", f.Name, f.Type.Name())
+	}
 	select {}
 }
