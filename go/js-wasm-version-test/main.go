b96f744ecf6f807a34f01793db188f336cd4e982
Jim Pick <jim@protocol.ai>
2020-02-10 15:38:42 -0800

    Tunnel websocket to WASM

--- go/js-wasm-version-test/main.go
+++ go/js-wasm-version-test/main.go
@@ -6,32 +6,39 @@ import (
 	"net/http"
 	"syscall/js"
 
-	"github.com/filecoin-project/lotus/api/client"
+	"github.com/filecoin-project/lotus/api-wasm/client"
 )
+
+func run(this js.Value, inputs []js.Value) interface{} {
+	go func() {
+		token := js.Global().Get("localStorage").Call("getItem", "token").String()
+		fmt.Println("Token:", token)
+		headers := http.Header{}
+		headers.Add("Authorization", "Bearer "+token)
+
+		// addr := "ws://localhost:8000/api/rpc/v0"
+		addr := "not used"
+		api, closer, err := client.NewFullNodeRPC(addr, headers)
+		if err != nil {
+			panic(err)
+		}
+		defer closer()
+		// fmt.Println("api", api)
+
+		ctx := context.Background()
+		v, err := api.Version(ctx)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("version", v.Version)
+	}()
+	return nil
+}
 
 func main() {
 	fmt.Println("Hello, WebAssembly!")
 
-	token := js.Global().Get("localStorage").Call("getItem", "token").String()
-	fmt.Println("Token:", token)
-	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+token)
+	js.Global().Get("ws").Call("start", js.FuncOf(run))
 
-	js.Global().Get("ws").Call("start")
-
-	// addr := "ws://localhost:8000/api/rpc/v0"
-	addr := "not used"
-	api, closer, err := client.NewFullNodeRPC(addr, headers)
-	if err != nil {
-		panic(err)
-	}
-	defer closer()
-	// fmt.Println("api", api)
-
-	ctx := context.Background()
-	v, err := api.Version(ctx)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("version", v.Version)
+	select {}
 }
--- go/js-wasm-version-test/websocket.js
+++ go/js-wasm-version-test/websocket.js
@@ -1,12 +1,25 @@
 function start (cb) {
-  // const ws = new WebSocket(`ws://${location.host}/api/rpc/v0`)
   console.log('Jim1')
-  /*
+  const ws = new WebSocket(`ws://${location.host}/api/rpc/v0`)
   ws.onopen = function (event) {
     console.log('Jim2')
+
+    function send (message) {
+      console.log('js send', message)
+      ws.send(message)
+    }
+
+    ws.onmessage = function (event) {
+      console.log('js recv', event.data)
+      if (window.ws.handler) {
+        window.ws.handler(event.data)
+      }
+    }
+
+    window.ws.send = send
+
     cb(ws)
   }
-  */
 }
 
 window.ws = {
