1214eff4bb687852af67cdfabaf9dc097643c7d5
Jim Pick <jim@protocol.ai>
2020-02-11 13:38:45 -0800

    Refactor into components

---
+++ demo-upload/chainHeight.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from '/web_modules/react.js'
+import ReactDOM from '/web_modules/react-dom.js'
+import { html } from '/web_modules/htm/react.js'
+import Client from '/lib/client-it.js'
+
+export default function ChainHeight (props) {
+  const [height, setHeight] = useState()
+
+  useEffect(() => {
+    async function run () {
+      const client = new Client({
+        url: `ws://${location.host}/api/rpc/v0`,
+        token: localStorage.getItem('token')
+      })
+      const source = client.chainNotify(setHeight)
+      for await (const changes of source) {
+        for (const change of changes) {
+          const { Type: changeType, Val: { Height: height }} = change
+          console.log(`Time: ${new Date()} Type: ${changeType} Height: ${height}`)
+          if (changeType === 'current' || changeType === 'apply') {
+            setHeight(height)
+          }
+        }
+      }
+    }
+    run()
+  }, [])
+
+  return html`
+    <div class="chainHeight">
+      Height: ${height}
+    </div>
+  `
+}
--- demo-upload/index.js
+++ demo-upload/index.js
@@ -1,66 +1,19 @@
-import { useEffect, useState, useRef } from '/web_modules/react.js'
+import { useState } from '/web_modules/react.js'
 import ReactDOM from '/web_modules/react-dom.js'
 import { html } from '/web_modules/htm/react.js'
-import Client from '/lib/client-it.js'
+import ChainHeight from './chainHeight.js'
+import FilePicker from './filePicker.js'
 
 function UploadDemo (props) {
-  const [height, setHeight] = useState()
   const [file, setFile] = useState()
-  const fileInput = useRef(null)
 
-  useEffect(() => {
-    async function run () {
-      const client = new Client({
-        url: `ws://${location.host}/api/rpc/v0`,
-        token: localStorage.getItem('token')
-      })
-      const source = client.chainNotify(setHeight)
-      for await (const changes of source) {
-        for (const change of changes) {
-          const { Type: changeType, Val: { Height: height }} = change
-          console.log(`Time: ${new Date()} Type: ${changeType} Height: ${height}`)
-          if (changeType === 'current' || changeType === 'apply') {
-            setHeight(height)
-          }
-        }
-      }
-    }
-    run()
-  }, [])
-
-  function handleFileChange (event) {
-    event.preventDefault()
-    console.log(
-      `Selected file - ${
-        fileInput.current.files[0].name
-      }`, fileInput
-    )
-    setFile(fileInput.current.files[0])
-  }
-
-  let info
-  if (file) {
-    info = html`
-      <div>
-        File: ${file.name}<br />
-        Size: ${file.size}<br />
-        Type: ${file.type}
-      </div>
-    `
-  }
   return html`
     <h1>Upload Demo</h1>
     <nav>
       <a href="/">Top</a>
     </nav>
-    <div>
-      Height: ${height}
-    </div>
-    <label>
-      Upload file:
-      <input type="file" ref=${fileInput} onChange=${handleFileChange} />
-    </label>
-    ${info}
+    <${ChainHeight} />
+    <${FilePicker} onFile=${setFile} />
   `
 }
 
