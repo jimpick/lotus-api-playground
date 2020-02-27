d70cfb5649f1b655616ac0c7ba67947857264dc8
Jim Pick <jim@protocol.ai>
2020-02-27 11:28:52 -0800

    Add file picker

---
+++ demo-upload/importer.js
@@ -0,0 +1,42 @@
+import { useState } from '/web_modules/react.js'
+import { html } from '/web_modules/htm/react.js'
+
+export default function Importer (props) {
+  const [file, setFile] = useState()
+  const [cid, setCid] = useState()
+
+  if (file != props.file) {
+    console.log('New file', props.file.name)
+    setFile(props.file)
+    setCid(null)
+  }
+
+  return html`
+    <div class="importer">
+      ${!cid && html`<button onClick=${doImport}>Import</button>`}
+      ${cid && html`CID: ${cid}`}
+    </div>
+  `
+
+  async function doImport () {
+    console.log('Import', file)
+    const token = localStorage.getItem('token')
+    const headers = {
+      'Content-Type': file.type,
+      Accept: '*/*',
+      Authorization: `Bearer ${token}`
+    }
+    const response = await fetch(
+      '/api/rest/v0/import',
+      {
+        method: 'PUT',
+        headers,
+        body: file
+      }
+    )
+    // FIXME: Check return code, errors
+    const result = await response.json()
+    console.log('Import result', result)
+    setCid(result.Cid['/'])
+  }
+}
--- demo-upload/index.js
+++ demo-upload/index.js
@@ -3,6 +3,7 @@ import ReactDOM from '/web_modules/react-dom.js'
 import { html } from '/web_modules/htm/react.js'
 import ChainHeight from './chainHeight.js'
 import FilePicker from './filePicker.js'
+import Importer from './importer.js'
 
 function UploadDemo (props) {
   const [file, setFile] = useState()
@@ -14,6 +15,7 @@ function UploadDemo (props) {
     </nav>
     <${ChainHeight} />
     <${FilePicker} onFile=${setFile} />
+    ${file && html`<${Importer} file=${file} />`}
   `
 }
 
