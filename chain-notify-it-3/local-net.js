59ef713834b7fe781776dfeb625069227cf01468
Jim Pick <jim@protocol.ai>
2020-03-14 20:42:00 +0000

    Get demo working that connects to 3 nodes

---
+++ chain-notify-it-3/local-net.js
@@ -0,0 +1,22 @@
+import ReactDOM from '/web_modules/react-dom.js'
+import { html } from '/web_modules/htm/react.js'
+import ChainNotify from './chain-notify.js'
+
+function LocalNet (props) {
+  return html`
+    <h1>Local Net Chain Notify (using async iterator)</h1>
+    <nav>
+      <a href="/">Top</a>
+    </nav>
+    <${ChainNotify} node="0" />
+    <${ChainNotify} node="1" />
+    <${ChainNotify} node="2" />
+  `
+}
+
+ReactDOM.render(
+  html`
+    <${LocalNet} />
+  `,
+  document.getElementById('app')
+)
