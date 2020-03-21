9e5e80f6de7e8ac628c286d6fb86de63f25f553d
Jim Pick <jim@protocol.ai>
2020-03-20 22:38:04 -0700

    SuspenseQuery prototype

--- ws-prototype/miner-panel2.js
+++ ws-prototype/miner-panel2.js
@@ -1,19 +1,33 @@
 import { useEffect, useState } from '/web_modules/react.js'
 import { html } from '/web_modules/htm/react.js'
-import suspenseDelayWithClient from './suspense-delay-with-client.js'
+import suspenseQuery from './suspense-query.js'
 
 export default function MinerPanel2 ({ node, miner }) {
-  // const [address, setAddress] = useState()
-  const [delay, setDelay] = useState()
-  useEffect(() => setDelay(suspenseDelayWithClient(1000, miner)), [miner])
+  const [query, setQuery] = useState()
+  useEffect(() => {
+    setQuery(suspenseQuery(async () => {
+      const address = await miner.actorAddress()
+      const sectorSize = await node.stateMinerSectorSize(address, [])
+      return {
+        address,
+        sectorSize
+      }
+    }))
+  }, [node, miner])
 
-  if (!delay) return null
-  const address = delay.read()
+  if (!query) return null
+  const {
+    address,
+    sectorSize
+  } = query.read()
 
   return html`
     <div>
       <div>
         Address: ${address}
+      </div>
+      <div>
+        Sector Size: ${sectorSize}
       </div>
     </div>
   `
